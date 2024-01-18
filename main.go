cfcfc615ec0583bf0341ee073f5832f2a889549a
Elena Samsonova <samsonova.elena@gmail.com>
2024-01-17 16:40:12 -0800

    Read input file and trip spaces

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
 func main() {
+	f, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+		os.Exit(1)
+	}
+	reader := bufio.NewReader(f)
+	for {
+		line, err := reader.ReadString('\n')
+		if err == io.EOF {
+			break
+		}
+		line = strings.TrimSpace(line)
+		fmt.Printf("%s\n", line)
+	}
 
 }
